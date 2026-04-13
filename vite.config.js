2c2476e4334e8628d99028c39c64fda3b5c34eb6
superquique <enriqueveintidos@gmail.com>
2026-04-13 09:40:52 -0600

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+<<<<<<< HEAD
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+=======
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+>>>>>>> 8a383b373b966478732bbed10ea3f5cde241d8b7
+})
