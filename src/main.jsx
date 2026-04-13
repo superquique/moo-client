c4098d55a9b08083b9b3266f0fa1db318ba76aee
superquique <enriqueveintidos@gmail.com>
2026-04-13 09:52:17 -0600

    feat: implement auth context and routing

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter as Router } from "react-router-dom";
+import { AuthProviderWrapper } from "./context/auth.context"; 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
