4242ebb54f4d6630f0d3418bda66fa8fdad8cb0a
superquique <enriqueveintidos@gmail.com>
2026-04-13 09:51:55 -0600

    feat: implement auth context and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,13 @@
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import IsPrivate from "./components/IsPrivate";
+import IsAnon from "./components/IsAnon";
+import SignupPage from './pages/SignupPage';
+import LoginPage from './pages/LoginPage';
+import SheetPage from './pages/SheetPage';
+import Navbar from './components/Navbar';
+import NotebookListPage from './pages/NotebookListPage';
+import NotebookPage from './pages/NotebookPage';
 
 
 function App() {
@@ -6,6 +15,20 @@ function App() {
   return (
     <div>
       
+      <Navbar />
+
+      <Routes>
+        <Route 
+          path="/signup" 
+          element={<IsAnon> <SignupPage /> </IsAnon>} 
+        />
+
+        <Route 
+          path="/login" 
+          element={<IsAnon> <LoginPage /> </IsAnon>} 
+        />
+
+      </Routes>
       
     </div>
   )
