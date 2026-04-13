39d1c7019c12e87c4b2edc6f5a30ee3723f982f8
superquique <enriqueveintidos@gmail.com>
2026-04-13 09:53:49 -0600

    feat: implement sheets service

---
+++ src/services/sheets.service.js
@@ -0,0 +1,56 @@
+import axios from 'axios';
+
+class SheetsService {
+  constructor() {
+    this.api = axios.create({
+      baseURL: import.meta.env.SERVER_URL || 'http://localhost:5005'
+    });
+
+    // Automatically set JWT token in the headers for every request
+    this.api.interceptors.request.use(config => {
+      // Retrieve the JWT token from the local storage
+      const storedToken = localStorage.getItem('authToken');
+
+      if (storedToken) {
+        config.headers = { Authorization: `Bearer ${storedToken}` };
+      }
+
+      return config;
+    });
+  }
+
+  // POST /api/sheets
+  createSheet = requestBody => {
+    return this.api.post('/api/sheets', requestBody);
+  };
+
+  // GET /api/sheets
+  getAllSheets = () => {
+    return this.api.get('/api/sheets');
+  };
+
+  // GET /api/sheets
+  getAllSheetsFromNotebook = (notebookId) => {
+    return this.api.get(`/api/sheets/notebook/${notebookId}`);
+  };
+
+  // GET /api/sheets/:id
+  getSheet = id => {
+    return this.api.get(`/api/sheets/${id}`);
+  };
+
+  // PUT /api/sheets/:id
+  updateSheet = (id, requestBody) => {
+    return this.api.put(`/api/sheets/${id}`, requestBody);
+  };
+
+  // DELETE /api/sheets/:id
+  deleteSheet = id => {
+    return this.api.delete(`/api/sheets/${id}`);
+  };
+}
+
+// Create one instance object
+const sheetsService = new SheetsService();
+
+export default sheetsService;
