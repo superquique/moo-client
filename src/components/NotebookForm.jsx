225b758163fc61c376594ebdd8df62d52a5a9db5
superquique <enriqueveintidos@gmail.com>
2026-04-14 07:33:29 -0600

    refactor: changed addnotebookform to notebook form and reuse it for editing notebooks

---
+++ src/components/NotebookForm.jsx
@@ -0,0 +1,76 @@
+import { useEffect, useRef, useState } from "react";
+
+function NotebookForm ({onSave, onCloseForm, existingNotebook = null}) {
+    const [name, setName] = useState("");
+    const dialogRef = useRef(null);
+
+    const pattern = /^[A-Za-z][A-Za-z0-9\- ]*$/;
+    const minLength = 3;
+    const maxLength = 60;
+
+    useEffect(() => {
+        if (existingNotebook) {
+            setName(existingNotebook.name);
+        } else {
+            setName("");
+        }
+    }, [existingNotebook]);
+
+    useEffect(() => {
+        dialogRef.current?.showModal();
+    }, []);
+
+    const handleSubmit = (e) => {
+        if (!name) {
+            return
+        }
+
+        if (name.length >= 3 && name.length <= 60 && pattern.test(name)){
+            onSave(name);
+            onCloseForm();
+        }
+    }
+    
+    const handleClose = () => {
+        setName("");
+    };
+
+    return (
+        <dialog
+            ref={dialogRef}
+            onClose={handleClose} 
+            id="add-notebook-modal" 
+            className="modal"
+        >
+            <div className="modal-box">
+                <h3 className="font-bold text-lg">New Notebook</h3>
+                <form method="dialog">
+                    <input 
+                        type="text" 
+                        className="input validator" 
+                        placeholder="Name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        pattern={pattern.source}
+                        minLength={minLength}
+                        maxLength={maxLength}
+                        required
+                    />
+                    <p className="validator-hint">
+                        Must be 3 to 60 characters
+                        <br />containing only letters, numbers or dash
+                    </p>
+                    <div className="modal-action">
+                        {/* if there is a button in form, it will close the modal */}
+                        <button onClick={e => onCloseForm(e)} className="btn">Cancel</button>
+                        <button onClick={e => handleSubmit(e)} className="btn">
+                            { existingNotebook ? 'Save' : 'Create'}
+                        </button>
+                    </div>
+                </form>                
+            </div>
+        </dialog>
+    )
+}
+
+export default NotebookForm;
--- src/pages/NotebookListPage.jsx
+++ src/pages/NotebookListPage.jsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react";
 import notebooksService from '../services/notebooks.service';
 import { Link } from "react-router-dom";
-import { FolderIcon, FolderPlusIcon, TrashIcon } from "@heroicons/react/24/outline";
-import AddNotebookForm from "../components/AddNotebookForm";
+import { FolderIcon, FolderPlusIcon, PencilSquareIcon, TrashIcon } from "@heroicons/react/24/outline";
+import NotebookForm from "../components/NotebookForm";
 
 
 function NotebookListPage () {
     const [notebooks, setNotebooks] = useState(null);
+    const [editingNotebook, setEditingNotebook] = useState(null);
+    const [isFormOpen, setIsFormOpen] = useState(false);
 
     const getNotebooks = () => {
         notebooksService.getAllNotebooks()
@@ -15,8 +17,22 @@ function NotebookListPage () {
     }
 
     const addNotebook = (name) => {
-        notebooksService.createNotebook({name: name})
+        notebooksService.createNotebook({name})
         .then((response) => {
+            getNotebooks();
+        })
+        .catch((error) => console.log(error));
+    }
+
+    const startEdition = (notebook) => {
+        setEditingNotebook(notebook);
+        openForm();
+    }
+
+    const editNotebook = (name, id) => {
+        notebooksService.updateNotebook(id, {name})
+        .then((response) => {
+            setEditingNotebook(null);
             getNotebooks();
         })
         .catch((error) => console.log(error));
@@ -32,7 +48,11 @@ function NotebookListPage () {
         })
     }
 
-    const onOpenForm = () => document.getElementById('add-notebook-modal').showModal()
+    const openForm = () => setIsFormOpen(true);
+    const closeForm = () => { 
+        setIsFormOpen(false);
+        setEditingNotebook(null);
+    };
 
     useEffect(() => {
         getNotebooks();
@@ -41,13 +61,29 @@ function NotebookListPage () {
     return (
         
         <div>
-            <AddNotebookForm onAddNotebook={addNotebook} />
+            { isFormOpen && 
+                <NotebookForm 
+                    key={editingNotebook?._id || "new"}
+                    onSave={
+                        (name) => {
+                            if (editingNotebook){
+                                editNotebook(name, editingNotebook._id);
+                            } else {
+                                addNotebook(name);
+                            }
+                        }
+                    }
+                    onCloseForm={closeForm}
+                    existingNotebook={editingNotebook}
+                />
+            }
+
             <ul className="list bg-base-100 rounded-box shadow-md">
   
                 <li className="p-4 pb-2 text-xs opacity-60 tracking-wide">
                     Notebooks
 
-                    <button onClick={onOpenForm} className="btn btn-square btn-ghost">
+                    <button onClick={openForm} className="btn btn-square btn-ghost">
                         <FolderPlusIcon className="size-6 text-black-500" />
                         {/* <svg className="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor"><path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"></path></g></svg> */}
                     </button>
@@ -68,6 +104,10 @@ function NotebookListPage () {
                         </div>
                         <button className="btn btn-square btn-ghost">
                             <svg className="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor"><path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"></path></g></svg>
+                        </button>
+                        <button className="btn btn-square btn-ghost" onClick={(e) => startEdition(notebook)}>
+                            <PencilSquareIcon className="size-5 text-black-500" />
+                            {/* <svg className="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor"><path d="M6 3L20 12 6 21 6 3z"></path></g></svg> */}
                         </button>
                         <button className="btn btn-square btn-ghost" onClick={(e) => deleteNotebook(notebook._id)}>
                             <TrashIcon className="size-5 text-black-500" />
