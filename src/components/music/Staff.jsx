bd41caed810541d74633c060f7bc438d45befbd2
superquique <enriqueveintidos@gmail.com>
2026-04-15 08:51:58 -0600

    feat: implement staff component

--- src/components/music/Staff.jsx
+++ src/components/music/Staff.jsx
@@ -1,9 +1,15 @@
+import React from 'react';
 import Note from "./Note";
+import Clef from "./Clef";
+import TimeSignature from "./TimeSignature";
+import { decodeNote } from "../../decoding/mooParser";
+import Barline from "./Barline";
 
-function Staff({notes, offset}) {
+function Staff({clef, timeSignature, measures, offset}) {
     const lines = [0, 1, 2, 3, 4]
-    const gap = 10;
-    const hGap = 40;
+    const gap = 5;
+    const hGap = 20;
+    let extraHGap = 0;
     const cIndex = 11;
     const noteIndexes = {
         'c': cIndex,
@@ -15,58 +21,68 @@ function Staff({notes, offset}) {
         'b': cIndex - 6
     }
 
-    // note = n#2'
-    const decodeNote = (note) => {
-        const match = note.match(/^([a-gA-G][#bn]?)(\d*)([,']*)/);
-
-        if (match) {
-            let [_, note, length, octave] = match;
-            
-            if (octave.includes(",")) {
-                octave = octave.length * -1;
-            } else {
-                octave = octave.length;
-            }
-
-            let accidental = "";
-
-            if (note.length > 1) {
-                accidental = note[1];
-            }
-
-            return {name: note, accidental, length, octave};
-        }
+    const clefIndexes = {
+        'bass': 2,
+        'treble': 10,
+        'alto': 4
     }
 
-    const decodedNotes = notes.map(note => decodeNote(note));
+    const bpm = timeSignature ? parseInt(timeSignature.split("/")[0]) : "";
+    const noteValue = timeSignature ? parseInt(timeSignature.split("/")[1]) : "";
 
+    if (clef) {
+        extraHGap += hGap;
+    }    
 
+    if (timeSignature) {
+        extraHGap += hGap;
+    }
+    
     return (
-        <svg height="100%" width="100%" xmlns="http://www.w3.org/2000/svg">
-
+        <svg x="0" y="0" width="100%" height="20%" viewBox="0 0 400 100" overflow="visible">
+            <rect width="100%" height="100%" fill="blue" fillOpacity="0.3" />
 
             {lines.map((i) => (
                 <line
-                key={i}
-                x1="0"
-                y1={ i * gap + offset }
-                x2="100%"
-                y2={ i * gap + offset }
-                style={{stroke: "#797979", strokeWidth: 2}}
+                    key={i}
+                    x1="0"
+                    y1={ `${i * gap + 40}` }
+                    x2="100%"
+                    y2={ `${i * gap + 40}` }
+                    style={{stroke: "#797979", strokeWidth: 1}}
                 />
             ))}
+
+            { clef && <Clef clef={clef} x="0" y="65" />}
+            { timeSignature && <TimeSignature bpm={bpm} noteValue={noteValue} x={hGap} y1={4 * gap/2 + 40} y2={8 * gap/2 + 40} />}
             
-            {decodedNotes.map((note, i) => (
-                <Note 
-                    key={i + lines.length}
-                    length={note.length}
-                    accidental={note.accidental}
-                    x={i * hGap}
-                    y={(noteIndexes[note.name[0]] - note.octave * 7) * gap/2 + offset}
-                    absIndex={noteIndexes[note.name[0]] - note.octave * 7}
-                    gap={gap}
-                />
-            ))}
+            {measures && measures.map((measure, i) => {
+                let currentX = 20;
+                return <svg key={`measure-${i}`} x={`${i * 25}%`} y="0" width="25%" 
+                overflow="visible">
+                    <rect x="0" y="0" width="100%" height="100%" fill="yellow" fillOpacity="0.3" />
+
+                    {measure.map((note, i) => {
+                        const xPos = currentX;
+
+                        let multiplier = bpm/note.length;
+                        console.log("multiplier", multiplier);
+                        currentX += (multiplier * hGap);
+
+                        return <Note 
+                            key={`note-${i}`}
+                            length={note.length}
+                            accidental={note.accidental}
+                            x={xPos}
+                            y={(noteIndexes[note.name[0]] - note.octave * 7) * gap/2 + 40}
+                            absIndex={noteIndexes[note.name[0]] - note.octave * 7}
+                            gap={gap}
+                        />
+                    })}
+
+                    <Barline barline="single" x="97" y={60}/>
+                </svg>
+º           })}
         
         </svg>
     )
