82772b54d0142ceff00c8fda9689ab85e146bfe9
superquique <enriqueveintidos@gmail.com>
2026-04-15 08:51:44 -0600

    feat: implement time signature component

--- src/components/music/TimeSignature.jsx
+++ src/components/music/TimeSignature.jsx
@@ -2,10 +2,11 @@ function TimeSignature ({bpm, noteValue, x, y1, y2}) {
 
     return (
         <>
-            <text x={x} y={y1} fill="black" fontSize="30">
+            <rect x="0" y="0" width="100%" height="100%" fill="green" fillOpacity="0.3" />
+            <text x={x} y="45" fill="black" fontSize="15" dominantBaseline="central">
                 {bpm}
             </text>
-            <text x={x} y={y2} fill="black" fontSize="30">
+            <text x={x} y="55" fill="black" fontSize="15" dominantBaseline="central">
                 {noteValue}
             </text>
         </>        
