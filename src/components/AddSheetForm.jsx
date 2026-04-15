40d038ec43b4e7b66f6ac713ab0a48ddb298daef
superquique <enriqueveintidos@gmail.com>
2026-04-14 18:08:45 -0600

    feat: add sheet form

--- src/components/AddSheetForm.jsx
+++ src/components/AddSheetForm.jsx
@@ -1,62 +1,115 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
-function AddSheetForm ({onAddSheet}) {
+function AddSheetForm ({onAddSheet, onCloseForm}) {
     const [title, setTitle] = useState("");
     const [clef, setClef] = useState("treble");
     const [timeSignature, setTimeSignature] = useState("4/4");
+    const dialogRef = useRef(null);
+
+    const pattern = /^[A-Za-z][A-Za-z0-9\- ]*$/;
+    const minLength = 3;
+    const maxLength = 60;
+    const validClefs = ["treble", "alto", "base"];
+    const validTimeSignatures = ["4/4", "3/4", "2/4"];
+
+    useEffect(() => {
+        dialogRef.current?.showModal();
+    }, []);
 
     const handleSubmit = (e) => {
         //e.preventDefault();
 
+        if (!title || !clef || !timeSignature) {
+            console.log(title, clef, timeSignature);
+            return;
+        }
+
+        if (title.length < minLength || title.length > maxLength || !pattern.test(title)) {
+            console.log(title.length < minLength , title.length > maxLength, pattern.test(title));
+            return;
+        }
+
+        if (!validClefs.includes(clef) || !validTimeSignatures.includes(timeSignature)) {
+            console.log(clef, timeSignature);
+            return;
+        }
+
         const payload = {
             title,
             clef,
-            timeSignature
+            timeSignature,
+            keySignature: "",
+            notes: "",
+            chords: "",
+            lyrics: ""
         }
 
+        console.log("adding sheet...");
         onAddSheet(payload);
+        dialogRef.current.close();
+        onCloseForm();
     }
     
     return (
-        <dialog id="add-notebook-modal" className="modal">
-            <div className="modal-box">
-                <h3 className="font-bold text-lg">New Sheet</h3>
-                <input 
-                    type="text" 
-                    className="input validator" 
-                    placeholder="Title"
-                    value={title}
-                    onChange={(e) => setTitle(e.target.value)}
-                    required 
-                />
-                <select 
-                    className="select"
-                    value={clef}
-                    onChange={(e) => setClef(e.target.value)}
-                >
-                    <option disabled={true}>Pick a clef</option>
-                    <option value="treble">𝄞 Treble</option>
-                    <option value="alto">𝄡 Alto</option>
-                    <option value="bass">𝄢 Bass</option>
-                </select>
-                <select 
-                    className="select"
-                    value={timeSignature}
-                    onChange={(e) => setTimeSignature(e.target.value)}
-                >
-                    <option disabled={true}>Pick a time signature</option>
-                    <option>4/4</option>
-                    <option>3/4</option>
-                    <option>2/4</option>
-                </select>
-                <div className="modal-action">
-                    <form method="dialog">
-                        {/* if there is a button in form, it will close the modal */}
-                        <button className="btn">Cancel</button>
+        <dialog 
+            ref={dialogRef} 
+            id="add-notebook-modal" 
+            className="modal"
+            onClose={onCloseForm}
+        >
+            <form method="dialog">
+                <div className="modal-box">
+                    <h3 className="font-bold text-lg">New Sheet</h3>
+
+                    <input 
+                        type="text" 
+                        className="input validator" 
+                        placeholder="Title"
+                        pattern={pattern.source}
+                        minLength={minLength}
+                        maxLength={maxLength}
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                        required 
+                    />
+                    <p className="validator-hint">
+                        Must be 3 to 60 characters
+                        <br />containing only letters, numbers or dash
+                    </p>
+                    
+                    <select 
+                        className="select"
+                        value={clef}
+                        onChange={(e) => setClef(e.target.value)}
+                    >
+                        <option disabled={true}>Pick a clef</option>
+                        <option value="treble">𝄞 Treble</option>
+                        <option value="alto">𝄡 Alto</option>
+                        <option value="bass">𝄢 Bass</option>
+                    </select>
+                    <select 
+                        className="select"
+                        value={timeSignature}
+                        onChange={(e) => setTimeSignature(e.target.value)}
+                    >
+                        <option disabled={true}>Pick a time signature</option>
+                        <option value="4/4">4/4</option>
+                        <option value="3/4">3/4</option>
+                        <option value="2/4">2/4</option>
+                    </select>
+                    <div className="modal-action">
+                       <button 
+                            type="button" 
+                            onClick={e => dialogRef.current.close()} 
+                            className="btn"
+                        >
+                            Cancel
+                        </button>
                         <button onClick={(e) => handleSubmit(e)} className="btn">Create</button>
-                    </form>
+                        
+                    </div>
                 </div>
-            </div>
+            </form>
         </dialog>
     )
 }
