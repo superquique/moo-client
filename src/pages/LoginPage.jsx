c69754d867ffce9607f27cfa7c6bbd4f7ca5c98c
superquique <enriqueveintidos@gmail.com>
2026-04-16 01:09:43 -0600

    feat: add css to login and signup

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -36,32 +36,79 @@ function LoginPage(props) {
   };
   
   return (
-    <div className="LoginPage">
-      <h1>Login</h1>
+    <div className="flex-grow flex items-center justify-center bg-base-100">
+      <div className="card w-100 bg-base-200 card-xl shadow-sm">
+        <div className="card-body">
+          <h2 className="card-title">Login</h2>
 
-      <form onSubmit={handleLoginSubmit}>
-        <label>Email:</label>
-        <input 
-          type="email"
-          name="email"
-          value={email}
-          onChange={handleEmail}
-        />
+          <form className="space-y-4" onSubmit={handleLoginSubmit}>
+            <label className="input validator">
+              <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <g
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                  strokeWidth="2.5"
+                  fill="none"
+                  stroke="currentColor"
+                >
+                  <rect width="20" height="16" x="2" y="4" rx="2"></rect>
+                  <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+                </g>
+              </svg>
+              <input 
+                type="email"
+                name="email"
+                value={email}
+                onChange={handleEmail} 
+                placeholder="Email"
+                required 
+              />
+            </label>
+            <div className="validator-hint hidden">Enter valid email address</div>
 
-        <label>Password:</label>
-        <input
-          type="password"
-          name="password"
-          value={password}
-          onChange={handlePassword}
-        />
+            <label className="input">
+              <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <g
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                  strokeWidth="2.5"
+                  fill="none"
+                  stroke="currentColor"
+                >
+                  <path
+                    d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"
+                  ></path>
+                  <circle cx="16.5" cy="7.5" r=".5" fill="currentColor"></circle>
+                </g>
+              </svg>
+              <input
+                type="password"
+                name="password"
+                value={password}
+                onChange={handlePassword}
+                placeholder="Password"                
+                required
+              />
+            </label>
+            <p className="validator-hint hidden">
+              Enter a password
+            </p>        
 
-        <button type="submit">Login</button>
-      </form>
-      { errorMessage && <p className="error-message">{errorMessage}</p> }
+            <div className="text-center">
+              <button className="btn btn-primary" type="submit">Login</button>
+            </div>
+          </form>
 
-      <p>Don't have an account yet?</p>
-      <Link to={"/signup"}> Sign Up</Link>
+          { errorMessage && <p className="text-1">{errorMessage}</p> }
+
+          <div className="justify-end items-center card-actions">
+            <p className="text-1">Don't have an account yet?</p>
+            <button className="btn">
+              <Link to={"/signup"}> Sign Up</Link>
+            </button>
+          </div>
+        </div>
+      </div>        
     </div>
   )
 }
--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -32,41 +32,114 @@ function SignupPage(props) {
 
   
   return (
-    <div className="SignupPage">
-      <h1>Sign Up</h1>
+    <div className="flex-grow flex items-center justify-center bg-base-100">
+      <div className="card w-100 bg-base-200 card-xl shadow-sm">
+        <div className="card-body">
+          <h2 className="card-title">Sign Up</h2>
 
-      <form onSubmit={handleSignupSubmit}>
-        <label>Email:</label>
-        <input 
-          type="email"
-          name="email"
-          value={email}
-          onChange={handleEmail}
-        />
+          <form className="space-y-4" onSubmit={handleSignupSubmit}>
+            <label className="input validator">
+              <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <g
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                  strokeWidth="2.5"
+                  fill="none"
+                  stroke="currentColor"
+                >
+                  <rect width="20" height="16" x="2" y="4" rx="2"></rect>
+                  <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+                </g>
+              </svg>
+              <input 
+                type="email"
+                name="email"
+                value={email}
+                onChange={handleEmail} 
+                placeholder="Email"
+                required 
+              />
+            </label>
+            <div className="validator-hint hidden">Enter valid email address</div>
 
-        <label>Password:</label>
-        <input 
-          type="password"
-          name="password"
-          value={password}
-          onChange={handlePassword}
-        />
+            <label className="input validator">
+              <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <g
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                  strokeWidth="2.5"
+                  fill="none"
+                  stroke="currentColor"
+                >
+                  <path
+                    d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"
+                  ></path>
+                  <circle cx="16.5" cy="7.5" r=".5" fill="currentColor"></circle>
+                </g>
+              </svg>
+              <input
+                type="password"
+                name="password"
+                value={password}
+                onChange={handlePassword}
+                placeholder="Password"
+                minLength="6"
+                pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}"
+                title="Password must have at least 6 characters and contain at least one number, one lowercase and one uppercase letter."
+                required
+              />
+            </label>
+            <p className="validator-hint hidden">
+              Password must have at least 6 characters, including
+              <br />At least one number <br />At least one lowercase letter <br />At least one uppercase letter
+            </p>        
 
-        <label>Name:</label>
-        <input 
-          type="text"
-          name="name"
-          value={name}
-          onChange={handleName}
-        />
+            <label className="input validator">
+              <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <g
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                  strokeWidth="2.5"
+                  fill="none"
+                  stroke="currentColor"
+                >
+                  <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path>
+                  <circle cx="12" cy="7" r="4"></circle>
+                </g>
+              </svg>
+              <input
+                type="text"
+                name="name"
+                value={name}
+                onChange={handleName}
+                placeholder="Username"
+                pattern="/^[A-Za-z0-9-]{3,30}$/"
+                minLength="3"
+                maxLength="30"
+                title="Only letters, numbers or dash"
+                required
+              />
+            </label>
+            <p className="validator-hint hidden">
+              Must be 3 to 30 characters
+              <br />containing only letters, numbers or dash
+            </p>
 
-        <button type="submit">Sign Up</button>
-      </form>
+            <div className="text-center">
+              <button className="btn btn-primary" type="submit">Sign Up</button>
+            </div>
+          </form>
 
-      { errorMessage && <p className="error-message">{errorMessage}</p> }
+          { errorMessage && <p className="error-message">{errorMessage}</p> }
 
-      <p>Already have account?</p>
-      <Link to={"/login"}> Login</Link>
+          <div className="justify-end items-center card-actions">
+            <p className="text-1">Already have an account?</p>
+            <button className="btn">
+              <Link to={"/login"}>Login</Link>
+            </button>
+          </div>
+        </div>
+      </div>        
     </div>
   )
 }
