6ab8195ce2c99b254377418a4790b32599185b35
superquique <enriqueveintidos@gmail.com>
2026-04-16 16:49:06 -0600

    feat: add favorite notbooks

--- src/pages/NotebookListPage.jsx
+++ src/pages/NotebookListPage.jsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
 import notebooksService from '../services/notebooks.service';
 import { Link } from "react-router-dom";
-import { FolderIcon, FolderPlusIcon, PencilSquareIcon, TrashIcon } from "@heroicons/react/24/outline";
+import { FolderIcon, FolderPlusIcon, HeartIcon, PencilSquareIcon, TrashIcon } from "@heroicons/react/24/outline";
 import NotebookForm from "../components/NotebookForm";
+import { HeartIcon as HeartIconSolid } from "@heroicons/react/24/solid";
 
 
 function NotebookListPage () {
@@ -46,6 +47,14 @@ function NotebookListPage () {
         .catch((error) => {
             console.log(error);
         })
+    }
+
+    const toggleFavorite = (id, isFavorite) => {
+        notebooksService.updateNotebook(id, {isFavorite: !isFavorite})
+        .then((response) => {
+            getNotebooks();
+        })
+        .catch((error) => console.log(error));
     }
 
     const openForm = () => setIsFormOpen(true);
@@ -111,8 +120,13 @@ function NotebookListPage () {
                                 <div className="text-xs uppercase font-semibold opacity-60"></div>
                             </Link>
                         </div>
-                        <button className="btn btn-square btn-ghost">
-                            <svg className="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor"><path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"></path></g></svg>
+                        <button onClick={() => toggleFavorite(notebook._id, notebook.isFavorite)} className="btn btn-square btn-ghost">
+                            {notebook.isFavorite ? 
+                                <HeartIconSolid className="size-5 text-secondary" />
+                                :
+                                <HeartIcon className="size-5 text-black-500" />
+                            }
+                            
                         </button>
                         <button className="btn btn-square btn-ghost" onClick={(e) => startEdition(notebook)}>
                             <PencilSquareIcon className="size-5 text-black-500" />
