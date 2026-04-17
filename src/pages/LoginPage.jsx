041125d895170b866bdd0940754a4937ccd7ad3a
superquique <enriqueveintidos@gmail.com>
2026-04-17 01:21:25 -0600

    refactor: change svg icons to heroicons

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,6 +2,7 @@ import { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/auth.context"; 
 import authService from "../services/auth.service";
+import { EnvelopeIcon, KeyIcon } from "@heroicons/react/24/outline";
 
 function LoginPage(props) {
   const [email, setEmail] = useState("");
@@ -43,18 +44,7 @@ function LoginPage(props) {
 
           <form className="space-y-4" onSubmit={handleLoginSubmit}>
             <label className="input validator">
-              <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                <g
-                  strokeLinejoin="round"
-                  strokeLinecap="round"
-                  strokeWidth="2.5"
-                  fill="none"
-                  stroke="currentColor"
-                >
-                  {/* <rect width="20" height="16" x="2" y="4" rx="2"></rect> */}
-                  <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
-                </g>
-              </svg>
+              <EnvelopeIcon className="size-4 text-black-500" />
               <input 
                 type="email"
                 name="email"
@@ -67,20 +57,7 @@ function LoginPage(props) {
             <div className="validator-hint hidden">Enter valid email address</div>
 
             <label className="input">
-              <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                <g
-                  strokeLinejoin="round"
-                  strokeLinecap="round"
-                  strokeWidth="2.5"
-                  fill="none"
-                  stroke="currentColor"
-                >
-                  <path
-                    d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"
-                  ></path>
-                  <circle cx="16.5" cy="7.5" r=".5" fill="currentColor"></circle>
-                </g>
-              </svg>
+              <KeyIcon className="size-4 text-black-500" />
               <input
                 type="password"
                 name="password"
--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import authService from "../services/auth.service";
+import { EnvelopeIcon, KeyIcon, UserIcon } from "@heroicons/react/24/outline";
 
 function SignupPage(props) {
   const [email, setEmail] = useState("");
@@ -39,18 +40,7 @@ function SignupPage(props) {
 
           <form className="space-y-4" onSubmit={handleSignupSubmit}>
             <label className="input validator">
-              <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                <g
-                  strokeLinejoin="round"
-                  strokeLinecap="round"
-                  strokeWidth="2.5"
-                  fill="none"
-                  stroke="currentColor"
-                >
-                  {/* <rect width="20" height="16" x="2" y="4" rx="2"></rect> */}
-                  <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
-                </g>
-              </svg>
+              <EnvelopeIcon className="size-4 text-black-500" />  
               <input 
                 type="email"
                 name="email"
@@ -63,20 +53,7 @@ function SignupPage(props) {
             <div className="validator-hint hidden">Enter valid email address</div>
 
             <label className="input validator">
-              <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                <g
-                  strokeLinejoin="round"
-                  strokeLinecap="round"
-                  strokeWidth="2.5"
-                  fill="none"
-                  stroke="currentColor"
-                >
-                  <path
-                    d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"
-                  ></path>
-                  <circle cx="16.5" cy="7.5" r=".5" fill="currentColor"></circle>
-                </g>
-              </svg>
+              <KeyIcon className="size-4 text-black-500" />
               <input
                 type="password"
                 name="password"
@@ -95,18 +72,7 @@ function SignupPage(props) {
             </p>        
 
             <label className="input validator">
-              <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                <g
-                  strokeLinejoin="round"
-                  strokeLinecap="round"
-                  strokeWidth="2.5"
-                  fill="none"
-                  stroke="currentColor"
-                >
-                  <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path>
-                  <circle cx="12" cy="7" r="4"></circle>
-                </g>
-              </svg>
+              <UserIcon className="size-4 text-black-500" />
               <input
                 type="text"
                 name="name"
