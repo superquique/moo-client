23c78f3840dd0ad3e025d0f88b68f75b41b8642a
superquique <enriqueveintidos@gmail.com>
2026-04-17 05:07:19 -0600

    fix: font imports and sizes

--- src/pages/SheetPage.jsx
+++ src/pages/SheetPage.jsx
@@ -130,14 +130,14 @@ function SheetPage ({}) {
         <>
             <section className="flex flex-col justify-center items-center">
                 <Toaster />
-                <div className="sticky top-16 z-100 bg-base-100 shadow-sm flex justify-between items-center w-full p-4 pb-2 tracking-wide">
+                <div className="sticky top-16 z-100 bg-base-100 shadow-sm flex justify-between items-center w-full p-4 tracking-wide">
                     
                     <div></div>
 
                     <input 
                         type="text" 
                         placeholder="Title" 
-                        className="input input-sm input-ghost w-128 text-xl font-bold text-center"
+                        className="input input-sm input-ghost w-128 text-lg font-bold text-center"
                         value={sheet.title}
                         onChange={(e) => onChange(e.target.value, "title")}
                     />
