f8d28a1f0ec66d95b4b54f1f4299346f38818e17
superquique <enriqueveintidos@gmail.com>
2026-04-16 20:50:12 -0600

    fix: app name collapsing on mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,19 @@
 import { Link, NavLink } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../context/auth.context";
-import { ArrowLeftStartOnRectangleIcon, FolderIcon, MusicalNoteIcon } from "@heroicons/react/24/outline";
+import { ArrowLeftStartOnRectangleIcon, ArrowRightEndOnRectangleIcon, FolderIcon, MusicalNoteIcon } from "@heroicons/react/24/outline";
 
 
 function Navbar () {
     const { isLoggedIn, user, logOutUser} = useContext(AuthContext);
 
     return (
-        <nav className="sticky top-0">
+        <nav className="sticky top-0 z-1000">
             <div className="max-lg:collapse bg-base-200 shadow-sm w-full rounded-md">
                 <input id="navbar-1-toggle" className="peer hidden" type="checkbox" />
                 <label htmlFor="navbar-1-toggle" className="fixed inset-0 hidden max-lg:peer-checked:block"></label>
                 <div className="collapse-title navbar">
-                    <div className="navbar-start">
+                    <div className="navbar-start w-full lg:w-1/2">
                         <label htmlFor="navbar-1-toggle" className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
@@ -54,23 +54,29 @@ function Navbar () {
                         </ul>
                     </div>
                     
-                    <div className="navbar-end hidden lg:flex">
-                        <ul className="menu menu-horizontal px-1">
-                            { !isLoggedIn && (
-                                <>
-                                    <li><NavLink to="/signup"> <button>Sign Up</button> </NavLink></li>
-                                    <li><NavLink to="/login"> <button>Login</button> </NavLink></li>
-                                </>
-                            )}
-                            { isLoggedIn && (
-                                <li>
-                                    <button onClick={logOutUser}>
-                                        <ArrowLeftStartOnRectangleIcon className="size-5 text-black-500" />
-                                        Logout
-                                    </button>
-                                </li>
-                            )}
-                        </ul>
+                    <div className="navbar-end hidden lg:flex gap-1">
+                        
+                        { !isLoggedIn && (
+                            <>
+                                
+                                <Link to="/signup"> 
+                                    <button className="btn btn-primary">Sign Up</button> 
+                                </Link>
+                            
+                                <NavLink to="/login"> 
+                                    <button className="btn" >Login</button> 
+                                </NavLink>
+                                
+                            </>
+                        )}
+                        { isLoggedIn && (
+                            
+                            <button className="btn" onClick={logOutUser}>
+                                Logout
+                            </button>
+                            
+                        )}
+                        
                     </div>
                 </div>
 
@@ -100,8 +106,18 @@ function Navbar () {
                         )}
                         { !isLoggedIn && (
                             <>
-                                <li><NavLink to="/signup"> <button>Sign Up</button> </NavLink></li>
-                                <li><NavLink to="/login"> <button>Login</button> </NavLink></li>
+                                <li>
+                                    <NavLink to="/signup">
+                                        
+                                        <button>Sign Up</button> 
+                                    </NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to="/login">
+                                        <ArrowRightEndOnRectangleIcon className="size-5 text-black-500" />
+                                        <button>Login</button> 
+                                    </NavLink>
+                                </li>
                             </>
                         )}
                     </ul>
